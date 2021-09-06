c5ffcb808ceee24669a90f2688e5fa41d4bb8707
Lucas <lucas.crenais@gmail.com>
2021-09-06 14:43:02 +0200

    2.5 compatibility

--- ChronopostHomeDelivery.php
+++ ChronopostHomeDelivery.php
@@ -48,14 +48,8 @@ class ChronopostHomeDelivery extends AbstractDeliveryModule
      */
     public function postActivation(ConnectionInterface $con = null): void
     {
-        try {
-            /** Security to not erase user configuration on reactivation */
-            ChronopostHomeDeliveryDeliveryModeQuery::create()->findOne();
-            ChronopostHomeDeliveryAreaFreeshippingQuery::create()->findOne();
-        } catch (\Exception $e) {
-            $database = new Database($con->getWrappedConnection());
-            $database->insertSql(null, array(__DIR__ . '/Config/thelia.sql'));
-        }
+        $database = new Database($con);
+        $database->insertSql(null, [__DIR__.'/Config/thelia.sql']);
 
         /** Default config values */
         $defaultConfig = [
